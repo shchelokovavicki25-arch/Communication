f37cbb9ffe628773cf48ed1d1a220763f366cfb8
shchelokovavicki25-arch <shchelokovavicki25@gmail.com>
2026-04-20 21:22:42 +0500

    Add files via upload

---
+++ новый 1.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/9fe8821e7db1b7fbbd036fa688e752f5.jpg",
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/37fe9c81cb299596581fe2bf8e715c68.jpg",
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/adbd5e000b4bb8fac6ce2ab01ff203c2.jpg",
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/bb5e18455309dc64911944ae53ac6c3d.jpg",
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/bd148a55947f9b2d2b220059c8b1014c.jpg",
+
+  "https://raw.githubusercontent.com/shchelokovavicki25-arch/Communication/refs/heads/main/6d7c32bdcbed5ef84a6f73f665e8a71c.jpg"
+
+]; 
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "THE FACE OF WAR",
+
+  "SLEEP",
+
+  "THE LAST SUPPER",
+
+  "PERSISTENCE OF MEMORY",
+
+  "GIRAFFE ON FIRE",
+
+  "ELEPHANTS"
+
+];
